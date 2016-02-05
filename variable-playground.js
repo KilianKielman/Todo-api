8f6232273c35d04d01157967afca4a252062b4aa
Kilian Kielman <kkielman@hotmail.com>
2016-02-05 15:57:30 +0100

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Kilian',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Kilian',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	// gradesArr = [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
